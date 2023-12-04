3261672ef07a3bc489ad34a9164e1033a453f9fa
Berkin Akaydin <berkin.akaydin@justeattakeaway.com>
2023-12-04 12:16:49 +0100

    input correction for d4

--- src/test/kotlin/Day4Test.kt
+++ src/test/kotlin/Day4Test.kt
@@ -9,10 +9,10 @@ class Day4Test {
     private val day = Day4()
     private var start: Long = 0L
 
-    init {
-        val input = FileReader().readFileAsLinesUsingBufferedReader("src/test/resources/small-input.txt")
-        day.input = input
-    }
+//    init {
+//        val input = FileReader().readFileAsLinesUsingBufferedReader("src/test/resources/small-input.txt")
+//        day.input = input
+//    }
 
     @Test
     fun question1() {
